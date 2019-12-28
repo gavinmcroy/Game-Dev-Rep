ce6b55de5355230e70e35e4311e54f86dd424699
Gavin Mcroy (Mac) <gavin.taylor.mcroy@gmail.com>
2019-12-29 05:49:45 +1030

    banging numbers

---
+++ Prototype/Assets/Scripts/Ar15.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using UnityEditor.UIElements;
+using UnityEngine;
+
+public class Ar15 : MonoBehaviour
+{
+    [SerializeField] private Transform playerPosition;
+
+
+    private void FixedUpdate()
+    {
+        var transform1 = this.transform;
+        var position = playerPosition.transform.position;
+        transform1.position = new Vector3(position.x, position.y, 0);
+    }
+}
